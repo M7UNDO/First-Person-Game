54bc90003aa7509bc7968f3f7856e2afd39b9e98
M7UNDO <dhlaminimfundo1@gmail.com>
2024-11-21 21:06:34 +0200

    Sfx lightning

--- First-Person-Game/Assets/Scripts/FirstPersonControls.cs
+++ First-Person-Game/Assets/Scripts/FirstPersonControls.cs
@@ -992,7 +992,7 @@ public class FirstPersonControls : MonoBehaviour
         {
             if (IntroUI[1].activeSelf == true)
             {
-                yield return new WaitForSeconds(4.0f);
+                yield return new WaitForSeconds(3.0f);
                 ContinueBtns[1].SetActive(true);
             }
         }
